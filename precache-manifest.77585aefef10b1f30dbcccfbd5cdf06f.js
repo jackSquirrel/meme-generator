a5ca4f9fbfaaa597e23cd974c566b4c28e02d845
Timur Ismagilov <ismagilov42@yandex.ru>
2020-09-23 01:53:36 +0300

    Updates

---
+++ precache-manifest.77585aefef10b1f30dbcccfbd5cdf06f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd1a15993351a1c3723d5bfcc3a971e9",
+    "url": "/meme-generator/index.html"
+  },
+  {
+    "revision": "e2637f3a2ded57711605",
+    "url": "/meme-generator/static/css/main.39d72d77.chunk.css"
+  },
+  {
+    "revision": "4831f0b1bfc17b3266e8",
+    "url": "/meme-generator/static/js/2.02143386.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/meme-generator/static/js/2.02143386.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2637f3a2ded57711605",
+    "url": "/meme-generator/static/js/main.1e2258a1.chunk.js"
+  },
+  {
+    "revision": "163fc7ea317d65653242",
+    "url": "/meme-generator/static/js/runtime-main.66d5941a.js"
+  }
+]);
