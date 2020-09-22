2129c5846b1141cc254240ae93ed9fbcb8cc0330
Timur Ismagilov <ismagilov42@yandex.ru>
2020-09-23 01:42:18 +0300

    Updates

---
+++ precache-manifest.7cf9554460cda4e9a181e2e9be48128d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "798fe3f23e5bcd080498cff27ae27f46",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2637f3a2ded57711605",
+    "url": "/static/css/main.39d72d77.chunk.css"
+  },
+  {
+    "revision": "4831f0b1bfc17b3266e8",
+    "url": "/static/js/2.02143386.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.02143386.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2637f3a2ded57711605",
+    "url": "/static/js/main.1e2258a1.chunk.js"
+  },
+  {
+    "revision": "3ee6d9289089d700c3ad",
+    "url": "/static/js/runtime-main.2d4e65bc.js"
+  }
+]);
